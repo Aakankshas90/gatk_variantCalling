ec431a433e3453670d1493b91a488483b8268ae6
Aakankshas90 <151615266+Aakankshas90@users.noreply.github.com>
2024-03-13 16:22:22 +0800

    Update VariantFiltration.sh

--- VariantFiltration.sh
+++ VariantFiltration.sh
@@ -8,7 +8,8 @@ snp_files="/RAID1/working/A423/aakanksha/SNPcalling/results/combinedVCF/SNP"
 indel_files="/RAID1/working/A423/aakanksha/SNPcalling/results/combinedVCF/indel"
 snp_extension="_snp.vcf.gz"
 indel_extension="_indels.vcf.gz"
-results="/RAID1/working/A423/aakanksha/SNPcalling/results/combinedVCF/SNP/VariantFiltration"
+results="/RAID1/working/A423/aakanksha/SNPcalling/results/VariantFiltration"
+log_file="${results}/error_log.txt"
 
 # Loop through all snp files with the specified extension
 for snp_file in "${snp_files}"/*"${snp_extension}"; do
@@ -22,7 +23,7 @@ for snp_file in "${snp_files}"/*"${snp_extension}"; do
         -R ${ref} \
         -V "${snp_file}" \
         -O ${results}/"${snp_name}"_snp_filt.vcf  \
-	2> ${results}/"${snp_name}"_snp_filt.txt \
+	2>> "${log_file}" \
         -filter-name "QD_filter" -filter "QD < 2.0" \
         -filter-name "QUAL30"  -filter "QUAL < 30.0" \
         -filter-name "FS_filter" -filter "FS > 60.0" \
@@ -34,14 +35,27 @@ for snp_file in "${snp_files}"/*"${snp_extension}"; do
         -genotype-filter-name "DP_filter" \
         -genotype-filter-expression "GQ < 10" \
         -genotype-filter-name "GQ_filter"
-        
+
+# Select SNPs that PASS filters
+gatk-4.5.0.0/gatk SelectVariants \
+	--exclude-filtered \
+	-V ${results}/"${snp_name}"_snp_filt.vcf \
+	-O ${results}/"${snp_name}"_snp_filtPASS.vcf \
+	2>> "${log_file}"
+
+# to exclude SNPs that failed genotype filters
+cat ${results}/"${snp_name}"_snp_filtPASS.vcf|grep \
+-v -E "DP_filter|GQ_filter" \
+> ${results}/"${snp_name}"_snp_filtPASS_filtGT.vcf \
+2>> "${log_file}"
+      
         echo "Processed sample: ${snp_name}"
     else
         echo "File not found: ${snp_file}"
     fi
 done
 
-# Loop through all indel files with the specified extension
+# Loop through all files with the specified extension
 for indel_file in "${indel_files}"/*"${indel_extension}"; do
     # Check if the file exists
     if [ -e "$indel_file" ]; then
@@ -53,7 +67,7 @@ for indel_file in "${indel_files}"/*"${indel_extension}"; do
         -R ${ref} \
         -V "${indel_file}" \
         -O ${results}/"${indel_name}"_indel_filt.vcf  \
-	2> ${results}/"${snp_name}"_indel_filt.txt \
+	2>> "${log_file}" \
         -filter-name "QD_filter" -filter "QD < 2.0" \
         -filter-name "QUAL30"  -filter "QUAL < 30.0" \
         -filter-name "FS_filter" -filter "FS > 60.0" \
@@ -62,6 +76,19 @@ for indel_file in "${indel_files}"/*"${indel_extension}"; do
         -genotype-filter-name "DP_filter" \
         -genotype-filter-expression "GQ < 10" \
         -genotype-filter-name "GQ_filter"
+
+# Select Indels that PASS filters
+gatk-4.5.0.0/gatk SelectVariants \
+	--exclude-filtered \
+	-V ${results}/"${indel_name}"_indel_filt.vcf \
+	-O ${results}/"${indel_name}"_indel_filtPASS.vcf \
+	2>> "${log_file}"
+
+# to exclude Indels that failed genotype filters
+cat ${results}/"${indel_name}"_indel_filtPASS.vcf|grep \
+-v -E "DP_filter|GQ_filter" \
+> ${results}/"${indel_name}"_indel_filtPASS_filtGT.vcf \
+2>> "${log_file}"
 
         echo "Processed sample: ${indel_name}"
     else
